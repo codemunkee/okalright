4e03e64756626898fb94690318a7440e27980de0
Russ Nealis <rnealis@vmware.com>
2015-06-08 16:26:34 -0700

    Added some fields for a recurring event table

--- reminder/models.py
+++ reminder/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Reminder(models.model):
+    reminder_name = models.CharField(max_length=200)
+    reminder_desc = models.CharField(max_length=1500)
+    date_added = models.DateTimeField('date reminder added')
+    date_reminder = models.DateTimeField('date of reminder')
+    recurs = models.BooleanField('false')
+    recurs_every = models.CharField(max_length=20)
